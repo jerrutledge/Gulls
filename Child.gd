2537a5ba9fd4d9ae98c277bc31a05cf91a60d1d9
noahmacaulay <usernameneeded@gmail.com>
2023-02-18 11:38:29 -0400

    child gull detection progress

--- Child.gd
+++ Child.gd
@@ -4,9 +4,10 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var speed = 250
+var speed = 50
 var velocity = 	Vector2.ZERO
 var origin = Vector2.ZERO
+var chasing = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,10 +18,20 @@ func _ready():
 func _process(delta):
 	
 	position += velocity * delta
-	if position.x - origin.x > 400.0:
+	if position.x - origin.x > 400.0 and chasing == false:
 		velocity.x = -speed
 		set_scale(Vector2(-1, 1))
 		
-	if position.x - origin.x < -400:
+	if position.x - origin.x < -400 and chasing == false:
 		velocity.x = speed
 		set_scale(Vector2(1,1))
+
+
+func _on_Vision_area_entered(area):
+	chasing = true
+	velocity.x *= 2
+	
+	
+func _on_Vision_area_exited(area):
+	chasing = false
+	velocity.x /= 2
