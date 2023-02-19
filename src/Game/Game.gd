3ab8b2191726995dcae6550b124ce7286175991a
Jeremy Rutledge <jerrutledge@users.noreply.github.com>
2023-02-19 17:20:15 -0400

    Implement level-finishing flow

--- src/Game/Game.gd
+++ src/Game/Game.gd
@@ -1,11 +1,6 @@
 extends Node2D
 
-
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-var _level_node = null
+var _level_node: Node2D = null
 var player = null
 export(String) var starting_level_name = "Tutorial"
 var current_level_name
@@ -15,12 +10,8 @@ onready var _pause_menu = $CanvasLayer/Pause
 onready var _death_screen = $CanvasLayer2/Death
 onready var timer = $HUD/TimerRect
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	
 	load_level(starting_level_name)
-	
 	_pause_menu.connect("restart", self, "_restart_level")
 
 
@@ -36,6 +27,7 @@ func _on_player_death():
 	_death_screen.show()
 
 func load_level(level_name):
+	get_tree().paused = false
 	if not (_level_node == null):
 		_level_node.name = _level_node.name + "old"
 		_level_node.queue_free()
@@ -50,5 +42,15 @@ func load_level(level_name):
 	current_level_name = level_name
 	_death_screen.hide()
 	timer.reset()
-	
 
+func finish_level():
+	get_tree().paused = true
+	$SuccessLayer.visible = true
+
+func next_level():
+	if _level_node.has_method("get_next_level"):
+		var new_level = _level_node.get_next_level()
+		print_debug(new_level)
+		load_level(new_level)
+	else:
+		print_debug("Can't get new level")
