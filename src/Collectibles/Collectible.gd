660df4587e7e210002ac23f0e1d00dce7c22d2ca
Jeremy Rutledge <jerrutledge@users.noreply.github.com>
2023-02-19 01:01:27 -0400

    implement item collection

--- src/Collectibles/Collectible.gd
+++ src/Collectibles/Collectible.gd
@@ -1,4 +1,19 @@
 extends Area2D
 
+var star_scene: PackedScene = preload("res://src/Collectibles/Stars.tscn")
+
 func collect():
+	var stars = star_scene.instance()
+	stars.position = position
+	stars.emitting = true
+	get_parent().call_deferred("add_child", stars)
 	queue_free()
+
+
+func _on_Player_body_entered(_body):
+	collect()
+
+
+func _on_Player_body_shape_entered(
+		_body_rid, _body, _body_shape_index, _local_shape_index):
+	collect()
