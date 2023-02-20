7d5894488d4f99916dc33c278b0747162a8e6b83
Jeremy Rutledge <jerrutledge@users.noreply.github.com>
2023-02-19 22:46:49 -0400

    Fix level loading issue

--- src/Game/Game.gd
+++ src/Game/Game.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var loading: bool = false
 var _level_node: Node2D = null
 var player: KinematicBody2D = null
 export(String) var starting_level_name = "Level"
@@ -37,10 +38,15 @@ func _on_player_death():
 	level_selector.show()
 
 func load_level(level_name):
+	if loading:
+		return
+	loading = true
 	reset_default_visibilities()
 	if not (_level_node == null):
 		_level_node.name = _level_node.name + "old"
 		_level_node.queue_free()
+		# wait for old level to de-load
+		yield(_level_node, "tree_exited")
 	if level_name != current_level_name:
 		level_resource = load("res://levels/" + level_name + ".tscn")
 	_level_node = level_resource.instance()
@@ -51,6 +57,7 @@ func load_level(level_name):
 	var _idc = player.connect("died", self, "_on_player_death")
 	current_level_name = level_name
 	timer.reset()
+	loading = false
 
 func finish_level():
 	get_tree().paused = true
